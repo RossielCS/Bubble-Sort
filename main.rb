41dbb88bd57e34e68ccb22838a57a4ba9fd13d81
Maclenn77 <maclenn77@gmail.com>
2020-04-09 17:54:30 -0500

    Added bubble_sort_by method

--- main.rb
+++ main.rb
@@ -9,9 +9,29 @@ def bubble_sort(numbers)
         if i == numbers.length-1
             counter += 1
             i = 0
-        end        
+        end
     end
     puts numbers
 end
 
 puts bubble_sort([45, 36, 18, 35, 13, 33, 25])
+
+def bubble_sort_by(arr)
+  i = 0
+  swapp = false
+  while arr[i+1].nil? == false do
+    if arr[i].length > arr[i+1].length
+      arr[i], arr[i+1] = arr[i+1], arr[i]
+      i += 1
+      swapp = true
+    elsif arr[i].length <= arr[i+1].length
+      i += 1
+    elsif arr[i+1].nil? == true && swapp
+      swapp = false
+      i = 0
+    end
+  end
+  puts arr
+end
+
+bubble_sort_by(["hi","hello","hey", "hooy"])
