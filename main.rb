a539dda1d6ae6da93bb38d7aac9073ff03868d09
Maclenn77 <maclenn77@gmail.com>
2020-04-10 10:54:20 -0500

    Adding yield command and solved a problem with the loop

--- main.rb
+++ main.rb
@@ -16,20 +16,23 @@ puts bubble_sort([45, 36, 18, 35, 13, 33, 25])
 
 def bubble_sort_by(arr)
   i = 0
-  swapp = false
-  while arr[i+1].nil? == false do
-    if arr[i].length > arr[i+1].length
-      arr[i], arr[i+1] = arr[i+1], arr[i]
-      i += 1
-      swapp = true
-    elsif arr[i].length <= arr[i+1].length
-      i += 1
-    elsif arr[i+1].nil? == true && swapp
-      swapp = false
-      i = 0
+  swapp = true
+  while swapp == true
+    swapp = false
+    while arr[i+1].nil? == false do
+      if arr[i].length > arr[i+1].length
+        arr[i], arr[i+1] = arr[i+1], arr[i]
+        i += 1
+        swapp = true
+      elsif arr[i].length <= arr[i+1].length
+        i += 1
+      end
     end
+    i = 0
   end
-  puts arr
+  yield arr
 end
+
+bubble_sort_by(["hi","hello","superfragilistic", "hey", "hooy", "A" ]) { |arr| puts "This is order by length #{arr}!" }
 
 #bubble_sort_by(["hi","hello","hey", "hooy"])
